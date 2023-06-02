e7b569d22de1c41caa98935ed0141cf0986d77d2
Asbern3333 <114946775+Asbern3333@users.noreply.github.com>
2023-06-02 11:30:23 -0400

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,11 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  let x1=0;
+  for(var i=0;i<nums.length;i++)
+  {
+      if(nums[i]%2==0)
+      x1++;
+  }
+  console.log(x1);
 }
 
 // Do not edit this line;
