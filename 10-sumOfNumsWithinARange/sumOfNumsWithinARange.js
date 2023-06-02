48a477b415a5ffe8873c82508819a1bc083150f1
Asbern3333 <114946775+Asbern3333@users.noreply.github.com>
2023-06-02 11:38:25 -0400

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,13 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+  let x1=0;
+  for(var i=0;i<nums.length;i++)
+  {
+      if(nums[i]>=start&&nums[i]<=end)
+      {x1++;
+      
+      }
+  }
+  console.log(x1);
 }
 
 // Do not edit this line;
