d31d46dfbfe9867afcf4bda2a0b65ec650154ea0
Asbern3333 <114946775+Asbern3333@users.noreply.github.com>
2023-06-02 11:32:02 -0400

    Update sumOfAllOddNumbers.js

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,13 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  let x1=0;
+  for(var i=0;i<nums.length;i++)
+  {
+      if(nums[i]%2==1)
+      x1++;
+  }
+  console.log(x1);
 }
+
 
 // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
