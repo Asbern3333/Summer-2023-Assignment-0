35aba78491b5c077cd39b6c5390a65eea7ef7ff6
Asbern3333 <114946775+Asbern3333@users.noreply.github.com>
2023-06-02 14:03:01 -0400

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,21 @@
-function frequencyCounter(word) {
-  // Insert code here;
+function frequencyCounter(str) {
+//Edge case: an empty array
+  if (str.length === 0) {
+    return {};
+  } 
+  var output = {};
+  var strArr = str
+//A loop
+  for (var i=0; i < strArr.length; i++) {
+    var word = strArr[i];
+    if (output[word] === undefined) {
+      output[word] = 1;
+    } else {
+      output[word] += 1;
+    }
+    
+  }
+  return output;
 }
 
 // Do not edit this line;
