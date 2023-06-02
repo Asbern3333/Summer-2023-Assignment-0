f020bb2372e807229fb9ad2a27bbba70f74b77c2
Asbern3333 <114946775+Asbern3333@users.noreply.github.com>
2023-06-02 10:19:25 -0400

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,18 @@
-function countOfAllBooleans(arr) {
-  // Insert code here;
+function f(arr)
+{
+    var x=0;
+    var i=0;
+    while(arr.length>=x)
+    {
+        
+        if(arr[x]==false||arr[x]==true)
+        {
+          i++  
+        }
+        
+        x++;
+    }
+    console.log(i);
 }
-
-// Do not edit this line;
-module.exports = countOfAllBooleans;
+var ar=new Array("123","123"," sadsad",false,true,12432,10,2,3,5,1,34,123,1232142);
+f(ar);
