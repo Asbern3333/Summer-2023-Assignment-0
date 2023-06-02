c6e9c656ecbae07e50e8de61b5f8761b4f05a58f
Asbern3333 <114946775+Asbern3333@users.noreply.github.com>
2023-06-02 11:51:31 -0400

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,6 +1,19 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  let x=new Array();
+  for(var i=start;i<=end;i++)
+  { if(i%3==0&&i%5==0)
+      x[i-start]="fizzBuzz";
+    else if(i%3==0)
+    x[i-start]="fizz";
+    else if(i%5==0)
+    x[i-start]="Buzz";
+    else 
+    x[i-start]=i;
+   
+  }
+  console.log(x);
 }
+
 
 // Do not edit this line;
 module.exports = fizzBuzz;
