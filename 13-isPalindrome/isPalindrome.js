4c086e63bbee38a7fbf7746b602ba99fb71fbac7
Asbern3333 <114946775+Asbern3333@users.noreply.github.com>
2023-06-02 13:00:08 -0400

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,14 @@
 function isPalindrome(word) {
-  // Insert code here;
+  var j=word.length-1;
+  for(var i=0;i<word.length;i++)
+  {
+      if(word[i]!=word[j])
+      {
+          return false;
+      }
+      j--;
+  }
+  return true;
 }
-
 // Do not edit this line;
 module.exports = isPalindrome;
